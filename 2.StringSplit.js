311deeb714bbd5c2edf720b775666981cfc34e07
BharaniNUI <bharanidaran.g@numentica-ui.com>
2024-03-05 17:07:35 +0530

    second commit

--- 2.StringSplit.js
+++ 2.StringSplit.js
@@ -1,18 +1,23 @@
-function stringSplit(string, num) {
-    let array = string.split("")
-    let resultArray = []
-
-    for (let i = 0; i < array.length; i + 2) {
-        let splitedString = ""
-        for (let j = 0; j < num; j++) {
-            if (array[i]) {
-                splitedString += array[i]
+function stringSplit(string, number) {
+    if(number<=string.length){
+        let array = string.split("")
+        let resultArray = []
+    
+        for (let i = 0; i < array.length; i + 2) {
+            let splitedString = ""
+            for (let j = 0; j < number; j++) {
+                if (array[i]) {
+                    splitedString += array[i]
+                }
+                i++
             }
-            i++
+            resultArray.push(splitedString)
         }
-        resultArray.push(splitedString)
+        return resultArray
+    }else{
+        return `Please enter number between 1 to ${string.length}`
     }
-    return resultArray
+
 }
 
 console.log(stringSplit("numenticaui", 2))
